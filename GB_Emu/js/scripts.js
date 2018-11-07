bdfc1dd791b39d1ae408bea641a0909dc1688f10
liamnajor <liamnajor@gmail.com>
2018-11-07 12:02:33 -0500

    Add files via upload

--- GB_Emu/js/scripts.js
+++ GB_Emu/js/scripts.js
@@ -1,1 +1,17 @@
-
+var el = document.getElementById("dummy")
+window.addEventListener("keydown", function(e){
+    console.log(e)
+    if(e.key === "a"){
+        memory.write("FF9B", "09")
+    }
+    if(e.key === "F12"){
+        memory.write("D050", "05")
+        memory.write("D051", "99")
+        memory.write("D055", "04")
+        memory.write("D053", "FF")
+        memory.write("D054", "FF")
+        memory.write("D081", "FF")
+        memory.write("D082", "FF")
+        memory.write("D045", "FF")
+    }
+})
