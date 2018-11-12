0c0b0c9588374733b75ee30a53d1a74a5f811dd8
liamnajor <liamnajor@gmail.com>
2018-11-12 11:45:14 -0500

    Update ROM.js

--- GB_Emu/js/ROM.js
+++ GB_Emu/js/ROM.js
@@ -45,82 +45,9 @@ Converter.stringHexadecimalToBytes = function(stringHexadecimal)
     	var dataAsBlob = new Blob([dataAsArrayBuffer], {type:'bytes'});
         return dataAsBlob
         }
-    var stringload = function(){
+    var stringload = function(e){
     var ROM = localStorage.getItem('ROM');
-    var bytes = ROM
-    var e = bytes.length
-    var p = 0
-    var i = e/2
-    var g = 0, d = 0, a = 0, x = 0, h = 0, w = 0, f = ""
-    while(d <= i){
-        if (g === 16){
-            g = 0
-            a += 1
-        }
-        if (a === 16){
-            a = 0
-            x += 1
-        }
-        if (x === 16){
-            x = 0
-            h += 1
-        }
-        if (h === 16){
-            h = 0
-            w += 1
-        }
-        f += "0x"+w.toString(16)+""+h.toString(16)+""+x.toString(16)+""+a.toString(16)+""+g.toString(16)+""
-        d += 16
-        g += 16
-    }
-    while(e >= 0){
-        var counter = e/2 -1
-        var b = e - 2
-        var res = bytes.substr(b, e)
-        c[counter] = res.substr(0, 2)
-        e = b
-    }
-    var t = c.length
-    var k = 0
-    var j = 0
-    var q = ""
-    while(t >= 16){
-        var u = j + 14
-        var n = f.substr(j, u)
-        var z = n.substr(0, 7)
-        q += ""+z+" "
-        var l = k + 15
-        j += 7
-        while(l >= k){
-            q += ""+c[k]+","
-            k += 1
-        }
-        q += " "
-        t -= 16
-    }
-    output = ""+q+""
-    var r = c.length - 147456 
-    var z = r / 16383
-    var y = ""+z+""
-    var b = y.split(".")
-    var value = ""
-    var str =  output
-    var e = str.split(" ");
-    var d = 1
-    var j = ""
-    while (d <= e.length){
-        j += ""+e[d]+""
-        d += 2
-    }
-    c = j.split(",")
-    //c[parseInt({address in hexidecimal}, 16) OR {address in base-10] = {replacement}
-    var i = 0
-    while(i <= bytes.length){
-        value += ""+c[i]+""
-        i += 1
-    }
-    var saver = Converter.stringHexadecimalToBytes(value)
-    c = saver
+    var saver = Converter.stringHexadecimalToBytes(ROM)
     var m = 262143
     var w = []
     var v = 0
@@ -129,7 +56,6 @@ Converter.stringHexadecimalToBytes = function(stringHexadecimal)
         v += 1
     }
     var blobby = blobToFile(save(w), "m2.gb")
-    
     FileLoad(blobby)
 }
     var memory = {
