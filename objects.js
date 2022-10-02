46661436f01e6327795a99fe0a6bf5aadc39f80b
liamnajor <liamnajor@gmail.com>
2022-10-02 19:18:05 -0400

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,106 @@
+var objectList = [
+"
+[4,'Skreek spawner'],
+
+[9,'Drivel'],
+
+[24,'Yumbo'],
+
+[39,'Chute Leech'],
+
+[72,'vertical(?) moheek'],
+
+[73,'horizontal(?) moheek'],
+
+[116,'Wallfire'],
+
+[257,'Septogg'],
+
+[260,'Moto'],
+
+[262,'Halzyn'],
+
+[263,'Ramulken'],
+
+[296,'Plasma Beam in Orb'],
+
+[297,'Plasma Beam'],
+
+[304,'Ice Beam in Orb'],
+
+[305,'Ice Beam'],
+
+[306,'Wave Beam in Orb'],
+
+[307,'Wave Beam'],
+
+[308,'Spazer in Orb'],
+
+[309,'Spazer'],
+
+[310,'Morph Ball Bombs in Orb'],
+
+[311,'Morph Ball bombs'],
+
+[312,'Screw Attack in Orb'],
+
+[313,'Screw Attack'],
+
+[320,'Varia Suit in Orb'],
+
+[321,'Varia Suit'],
+
+[322,'High Jump in Orb'],
+
+[323,'High Jump'],
+
+[324,'Space Jump in Orb'],
+
+[325,'Space Jump'],
+
+[326,'Spider Ball in Orb'],
+
+[327,'Spider Ball'],
+
+[328,'Spring Ball in Orb'],
+
+[329,'Spring Ball'],
+
+[336,'Energy Tank in Orb'],
+
+[337,'Energy Tank'],
+
+[338,'Missile Tank in Orb'],
+
+[339,'Missile Tank'],
+
+[340,'Blob Thrower'],
+
+[341,'Energy Restore Orb'],
+
+[342,'Arachnus'],
+
+[343,'Missile Restore'],
+
+[352,'Alpha Metroid, pre shed'],
+
+[354,'Metroid Egg'],
+
+[355,'Gamma Metroid'],
+
+[356,'Alpha Metroid, post shed'],
+
+[358,'Metroid Egg Clone'],
+
+[371,'Zeta Metroid, pre shed'],
+
+[377,'Omega Metroid'],
+
+[518,'Larva Metroid'],
+
+[520,'Flitt'],
+
+[521,'Meboid(?)'],
+
+[534,'Gullugg']
+]
