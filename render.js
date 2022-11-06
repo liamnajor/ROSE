80c58cf46173cd55d32509eaf05e54a4d1a357a3
liamnajor <liamnajor@gmail.com>
2022-11-06 18:02:58 -0500

    Update render.js

--- render.js
+++ render.js
@@ -742,7 +742,7 @@ var rawObjects = []
     while(e != 4){
             var p = d*4
             if(byteArray[loc + p] != "ff"){
-                objects[d] = "Object "+d+" - ID:"+byteArray[loc + p]+", Type:"+logObjectList[parseInt(byteArray[loc + p + 1],16)]+", X:"+byteArray[loc + p + 2]+", Y:"+byteArray[loc + p + 3]+"</p>"
+                objects[d] = "Object "+d+" - ID:"+byteArray[loc + p]+", Type:"+logObjectList[parseInt(byteArray[loc + p + 1],16)]+":"+byteArray[loc + p + 1]+", X:"+byteArray[loc + p + 2]+", Y:"+byteArray[loc + p + 3]+"</p>"
                 d += 1
             } else {
                 e = 4
